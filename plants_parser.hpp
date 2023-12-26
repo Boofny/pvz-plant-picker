1abc33be1f0b77ee4d3d0d8430aa6c3b1c7eded2
Danin Sadžak <daninsadzak@gmail.com>
2023-12-26 21:28:17 +0100

    Implemented plants_parser.hpp

---
+++ plants_parser.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector<std::string> parse_plants() {
+    std::ifstream plants_input("plants.txt");
+
+    if (!plants_input.is_open()) std::cout << "Error! Could not open file.\n";
+    else {
+        std::vector<std::string> plants;
+        std::string element;
+    
+        while (plants_input >> element) {
+            plants.push_back(element);
+        }
+
+        plants_input.close();
+
+        return plants;
+    }
+}
--- script.cpp
+++ script.cpp
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include "plants_parser.hpp"
 
 int main() {
-    printf("Hello, World!\n");
+    std::vector<std::string> plants = parse_plants();
+
+    for(int i = 0; i < plants.size(); i++) {
+        std::cout << "PLANT NAME: " << plants[i] << '\n';
+    }
     return 0;
 }
