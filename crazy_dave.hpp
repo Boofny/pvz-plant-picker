44823f21bc890a4826dd7e5b7d19be264a83a3b8
Danin Sadžak <daninsadzak@gmail.com>
2024-01-04 14:57:58 +0100

    crazy_dave.hpp now parses crazy dave's plants

--- crazy_dave.hpp
+++ crazy_dave.hpp
@@ -2,12 +2,17 @@
 #include "plants_parser.hpp"
 
 std::vector<std::string> crazy_dave() {
+    std::ifstream dave("crazy_dave_plants.txt");
     std::vector<std::string> crazyDavePlants;
     std::string plant;
-    for(int i = 1; i <= 3; i++) {
-        std::cout << "Enter the " << i + 1 << " Crazy Dave plant!\n";
-        std::cin >> plant;
-        crazyDavePlants.push_back(plant);
+
+    if (!dave.is_open()) std::cout << "Trouble opening crazy_dave_plants.txt!\n";
+    else {
+        while (dave >> plant) {
+            crazyDavePlants.push_back(plant);
+        }
+
+        dave.close();
     }
 
     return crazyDavePlants;
