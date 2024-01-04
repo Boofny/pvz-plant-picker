11f663a508e91694e5ca0eef780c0e5384db9a83
Danin Sadžak <daninsadzak@gmail.com>
2024-01-04 18:28:39 +0100

    made generation for early game levels

--- New Game/generation.hpp
+++ New Game/generation.hpp
@@ -20,3 +20,24 @@ std::vector<std::string> parse_plants() {
 
     return parsed_plants;
 }
+
+void randomizer() {
+    srand(time(NULL));
+    std::vector<std::string> plants = parse_plants();
+    
+    if (plants.size() <= 6) {
+        int action_performed {0};
+        while (true) {
+            int generator_plants = rand() % plants.size();
+            int generator_lanes  = rand() % 5 + 1;
+            if ((GetAsyncKeyState(VK_RETURN) & 0x8001) && !(action_performed)) {
+                std::cout << "PUT PLANT " << plants[generator_plants] << " ON LANE " << generator_lanes << '\n';
+                action_performed = 1;
+            } else if (!(GetAsyncKeyState(VK_RETURN)) & 0x8001) {
+                action_performed = 0;
+            }
+        }   
+    } else {
+        //For levels after getting 7 or more plants
+    }
+}
