1c0b690dcd00b2977cfe0e0e6ef61cad57c71092
Danin Sadžak <daninsadzak@gmail.com>
2024-01-04 18:13:37 +0100

    tidied up script.cpp

--- New Game/script.cpp
+++ New Game/script.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include "level1_2.hpp"
+#include "level1_3.hpp"
 
 int main() {
-    randomizer_level2();
+    int level;
+    std::cout << "On what level are you on lol?\n";
+    std::cin >> level;
+
+    switch (level) {
+        case 2:
+            randomizer_level2();
+            break;
+        case 3:
+            randomizer_level3();
+    }
     return 0;
 }
