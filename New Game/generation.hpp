4040a5213e9d7427db30a88a461b6b2d31795ff9
Danin Sadžak <daninsadzak@gmail.com>
2024-01-04 19:01:13 +0100

    made generation for plant size greater than 6

--- New Game/generation.hpp
+++ New Game/generation.hpp
@@ -38,6 +38,48 @@ void randomizer() {
             }
         }   
     } else {
-        //For levels after getting 7 or more plants
+        // For levels after getting 7 or more plants
+
+        std::vector<std::string> randomized_plants;
+        int seed_slot = 6;
+
+        for (int i = 0; i < seed_slot; i++) {
+            int random_number = rand() % plants.size();
+            std::string selected_plant = plants[random_number];
+
+            bool is_duplicate = false;
+            for (const std::string& existing_plant : randomized_plants) {
+                if (existing_plant == selected_plant) {
+                    is_duplicate = true;
+                    break;
+                }
+            }
+
+            if (!is_duplicate) {
+                randomized_plants.push_back(selected_plant);
+            }
+        }
+
+        for(int i = 0; i < seed_slot; i++) {
+            std::cout << "SELECT PLANT: " << randomized_plants[i] << '\n';
+            Sleep(1000);
+        }
+
+        std::cout << "PRESS ENTER TO CONTINUE.\n";
+        if (GetAsyncKeyState(VK_RETURN) & 0x8001) {}
+        std::cout << '\n';
+
+        int action_performed {0};
+        while (true) {
+            int generator_plants = rand() % seed_slot;
+            int generator_lanes  = rand() % 5 + 1;
+            if ((GetAsyncKeyState(VK_RETURN) & 0x8001) && !(action_performed)) {
+                std::cout << "PUT PLANT " << randomized_plants[generator_plants] << " ON LANE " << generator_lanes << '\n';
+                action_performed = 1;
+            } else if (!(GetAsyncKeyState(VK_RETURN)) & 0x8001) {
+                action_performed = 0;
+            }
+        }
+
     }
 }
