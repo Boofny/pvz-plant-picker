c04abf9aae90af3d68bfa8e1dc4e6d5e2f4dc65f
Danin Sadžak <daninsadzak@gmail.com>
2024-01-04 17:59:36 +0100

    Made randomizer for level 1-2

---
+++ New Game/level1_2.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <windows.h>
+
+std::vector<std::string> available_plants = {
+    "Peashooter",
+    "Sunflower"
+};
+
+void randomizer_level2() {
+    srand(time(NULL));
+
+    int action_performed {0};
+    while (true) {
+        int generator_plants = rand() % 2;
+        int generator_lanes  = rand() % 3 + 1;
+        if ((GetAsyncKeyState(VK_RETURN) & 0x8001) && !(action_performed)) {
+            std::cout << "PUT PLANT " << available_plants[generator_plants] << " ON LANE " << generator_lanes << '\n';
+            action_performed = 1;
+        } else if (!(GetAsyncKeyState(VK_RETURN)) & 0x8001) {
+            action_performed = 0;
+        }
+    }
+}
